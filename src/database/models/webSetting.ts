78d72ef1bff017522897a4b59d3ab46a0cd2a3ad
Palakduofusion <gohelpalak14@gmail.com>
2025-09-08 12:55:00 +0530

    commit

--- src/database/models/webSetting.ts
+++ src/database/models/webSetting.ts
@@ -1,22 +1,24 @@
+import { string } from "joi";
+
 var mongoose = require('mongoose')
 
 const webSettingSchema = new mongoose.Schema({
-    name:{ type: String},
-    email :{ type: String},
-    phoneNumber: { type: Number},
-    razorpayKeyId : { type: String},
-    razorpayKeySecret : { type: String},    
-
-      socialMedia: {
-        instagram: { type: String },
-        facebook: { type: String }, 
-        linkedin: { type: String },
-        twitter: { type: String },
-        youtube: { type: String }
-    },    
-    isDeleted: { type: Boolean, default: false },
-    isBlocked: { type: Boolean, default: false }
+  name: { type: String },
+  email: { type: String },
+  phoneNumber: { type: Number },
+  razorpayKeyId: { type: String },
+  razorpayKeySecret: { type: String },
+  ourStudent: { type: String, default: 0 },
+  rating: { type: String, default: 0 },
+  socialMedia: {
+    instagram: { type: String },
+    facebook: { type: String },
+    linkedin: { type: String },
+    twitter: { type: String },
+    youtube: { type: String }
+  },
+  isDeleted: { type: Boolean, default: false },
+  isBlocked: { type: Boolean, default: false }
 }, { timestamps: true, versionKey: false })
 
 export const webSettingModel = mongoose.model('profileSetting', webSettingSchema);
-    
